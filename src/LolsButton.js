980f9fa8f37cdedeaa315321efa29fe6065279ee
anderskk <Mittgithubpass0rd>
2019-05-23 21:44:38 +0200

    lols

--- src/LolsButton.js
+++ src/LolsButton.js
@@ -4,25 +4,34 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 
 const randomNumber = max => Math.floor(Math.random() * max);
-const LolsButton = ({ onClick }) => {
+const LolsButton = ({ onClick, disabled }) => {
 
     const [position, setPosition] = useState();
-    const positionStyle = () => {
-        position: 'absolute'
-    };
+    const positionStyle = () => ({
+        position: 'absolute',
+        left: `${Math.floor(position.x * window.innerWidth)}px`,
+        top: `${Math.floor(position.y * window.innerHeight)}px`
+    });
 
     return (
         <Button
-            style={ position ?  }
+            style={ position && positionStyle() }
             onClick={ () => {
                 onClick();
                 setPosition(undefined);
             } }
-            onMouseEnter={ () => setPosition({ x: randomNumber(90), y: randomNumber(90) }) }
-         />
+            onMouseEnter={ () => setPosition({ x: Math.random(0.9), y: Math.random(0.9) }) }
+            variant="outline-primary"
+            size="lg"
+            hidden={ disabled }
+            disabled={ disabled }>
+            { 'Start' }
+        </Button>
     );
 };
 
 LolsButton.propTypes = {
     onClick: PropTypes.func.isRequired
 };
+
+export default LolsButton;
